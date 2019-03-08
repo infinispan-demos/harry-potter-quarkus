80b1960936fd3de21f6156b601006d8d5aa7b9e1
Katia Aresti <karesti@redhat.com>
2019-03-08 18:17:52 +0100

    readme and some small logs

--- src/main/java/org/infinispan/hp/service/HogwartsMagicCreator.java
+++ src/main/java/org/infinispan/hp/service/HogwartsMagicCreator.java
@@ -7,10 +7,10 @@ import java.util.concurrent.TimeUnit;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.hp.model.HPCharacter;
 import org.infinispan.hp.model.HPMagic;
 import org.infinispan.hp.model.HPSpell;
-import org.infinispan.client.hotrod.RemoteCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,11 +40,10 @@ public class HogwartsMagicCreator {
     * Wait 10 seconds (should be enough time for stores to be created if they don't exist. Then perform some magic every
     * 3 seconds
     */
-   @Scheduled(every = "3s", delay = 10, delayUnit = TimeUnit.SECONDS)
+   @Scheduled(every = "3s", delay = 5, delayUnit = TimeUnit.SECONDS)
    void executeMagic() {
-      if (characters == null || spells == null || magic == null) {
-         LOGGER.error("Unable to perform magic at Hogwarts ... Is You-Know-Who around?");
-         throw new IllegalStateException("Characters, Spells or Magic stores are null. Try restarting the application");
+      if (illegalState()) {
+         throw new IllegalStateException("Unable to perform magic at Hogwarts ... Is You-Know-Who around?");
       }
 
       LOGGER.info("... magic is happening ...");
@@ -58,7 +57,7 @@ public class HogwartsMagicCreator {
 
       HPSpell spell = getRandomHpSpell();
 
-      if (character == null || spell == null){
+      if (character == null || spell == null) {
          LOGGER.info("Unable to perform magic, character or spell are null");
          return;
       }
@@ -66,6 +65,20 @@ public class HogwartsMagicCreator {
       // Perform magic
       String id = UUID.randomUUID().toString();
       magic.put(id, new HPMagic(id, character.getName(), spell.getName(), character.isAtHogwarts()));
+   }
+
+   private boolean illegalState() {
+      if (characters == null || spells == null || magic == null) {
+         LOGGER.error("Characters, Spells or Magic stores are null. Expecto Patronum and restart the application");
+         return true;
+      }
+
+      if (characters.size() == 0 || spells.size() == 0) {
+         LOGGER.error("Characters or Spells stores are empty. Lumos and restart the application");
+         return true;
+      }
+
+      return false;
    }
 
    private HPSpell getRandomHpSpell() {
