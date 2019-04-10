01936a1cea67221e16248efa275661068dbc0a64
Katia Aresti <karesti@redhat.com>
2019-04-10 15:53:35 +0200

    change doc

---
+++ adoc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#Generate diagrams
+asciidoctor -r asciidoctor-diagram -a generate-diagrams README.adoc
+
+#Generate HTML
+asciidoctor -a source-highlighter=pygments README.adoc
---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -x
+git commit -a -m "Updated docs"
+set -e
+git push origin master
+
+rm -rf /tmp/images
+
+#Generate diagrams
+asciidoctor -r asciidoctor-diagram -a generate-diagrams README.adoc
+
+#Generate HTML
+asciidoctor -a source-highlighter=pygments README.adoc
+
+mv images /tmp
+
+git checkout -f gh-pages
+mv /tmp/images/* images/
+mv README.html index.html
+git add -A
+git commit -m "Updated docs"
+git push origin gh-pages
+git checkout master
