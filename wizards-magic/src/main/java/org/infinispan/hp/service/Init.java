661fe09c6c091e57b84bd08371a7af1dbb0344b0
Katia Aresti <karesti@redhat.com>
2019-09-14 22:34:33 +0200

    upgrade version and make tests pass

--- wizards-magic/src/main/java/org/infinispan/hp/service/Init.java
+++ wizards-magic/src/main/java/org/infinispan/hp/service/Init.java
@@ -6,6 +6,8 @@ import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.commons.configuration.BasicConfiguration;
+import org.infinispan.commons.configuration.XMLStringConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,11 +25,19 @@ public class Init {
    @Inject
    RemoteCacheManager cacheManager;
 
+   private static final String CACHE_CONFIG =
+          "<infinispan><cache-container>" +
+                  "<distributed-cache name=\"%s\">" +
+                  "<encoding><key media-type=\"text/plain\"/><value media-type=\"application/json\"/></encoding>" +
+                  "<expiration interval=\"10000\" lifespan=\"10\" max-idle=\"10\"/></distributed-cache>" +
+                  "</cache-container></infinispan>";
+
    /**
     * Listens startup event to load the data
     */
    void onStart(@Observes @Priority(value = 1) StartupEvent ev) {
       LOGGER.info("On start - clean and load");
-      cacheManager.administration().getOrCreateCache(HP_MAGIC_NAME, "default");
+      String xml = String.format(CACHE_CONFIG, "magic");
+      cacheManager.administration().getOrCreateCache(HP_MAGIC_NAME, new XMLStringConfiguration(xml));
    }
 }
