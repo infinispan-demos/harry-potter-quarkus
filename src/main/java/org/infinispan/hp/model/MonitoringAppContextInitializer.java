058f9e6fa842264c5b5805185a21abe242da5694
Katia Aresti <karesti@redhat.com>
2022-02-15 16:42:21 +0100

    upgrade

--- src/main/java/org/infinispan/hp/model/MonitoringAppContextInitializer.java
+++ src/main/java/org/infinispan/hp/model/MonitoringAppContextInitializer.java
@@ -1,9 +1,10 @@
 package org.infinispan.hp.model;
 
+import org.infinispan.protostream.GeneratedSchema;
 import org.infinispan.protostream.SerializationContextInitializer;
 import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
 
 @AutoProtoSchemaBuilder(schemaPackageName = "hp_monitoring",
       includeClasses = {HPCharacter.class, HPSpell.class, HPMagic.class, CharacterType.class})
-public interface MonitoringAppContextInitializer extends SerializationContextInitializer {
+public interface MonitoringAppContextInitializer extends GeneratedSchema {
 }
