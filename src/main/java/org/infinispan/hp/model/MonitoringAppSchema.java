0ede38d8523e3d55498f8409239c9c169d1e6088
Katia Aresti <karesti@redhat.com>
2024-06-26 17:20:32 +0200

    remove the need of proto adapter

--- src/main/java/org/infinispan/hp/model/MonitoringAppSchema.java
+++ src/main/java/org/infinispan/hp/model/MonitoringAppSchema.java
@@ -4,6 +4,9 @@ import org.infinispan.protostream.GeneratedSchema;
 import org.infinispan.protostream.annotations.ProtoSchema;
 
 @ProtoSchema(schemaPackageName = "hp_monitoring",
-      includeClasses = {HPCharacter.class, HPSpell.class, HPMagic.class, CharacterType.class, CustomUUIDAdapter.class})
+        includeClasses = {HPCharacter.class, HPSpell.class, HPMagic.class, CharacterType.class},
+        dependsOn = {
+                org.infinispan.protostream.types.java.CommonTypes.class
+        })
 public interface MonitoringAppSchema extends GeneratedSchema {
 }
